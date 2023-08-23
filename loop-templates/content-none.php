b2b82bb553c080c2887b46e38e3ab2e5ff55a0c8
Pat Monette <patrick@ontarioseo.ca>
2023-08-23 17:04:04 -0400

    search translations

--- loop-templates/content-none.php
+++ loop-templates/content-none.php
@@ -9,13 +9,21 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+if(lang_en()) {
+	$no_results_txt = 'Nothing Found';
+	$no_results_desc = 'Sorry, but nothing matched your search terms. Please try again with some different keywords.';
+} elseif(lang_fr()) {
+	$no_results_txt = 'Aucun résultat';
+	$no_results_desc = 'Désolé, mais rien ne correspond à vos termes de recherche. Veuillez réessayer avec des mots-clés différents.';
+}
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found my-6" class="uk-animation-slide-bottom-medium">
 
 	<header class="page-header">
 
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'understrap' ); ?></h1>
+		<h1 class="page-title"><?php esc_html_e( $no_results_txt, 'understrap' ); ?></h1>
 
 	</header><!-- .page-header -->
 
@@ -35,7 +43,7 @@ defined( 'ABSPATH' ) || exit;
 
 			printf(
 				'<p>%s<p>',
-				esc_html__( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'understrap' )
+				esc_html__( $no_results_desc, 'understrap' )
 			);
 			get_search_form();
 
--- searchform.php
+++ searchform.php
@@ -15,12 +15,18 @@ $aria_label = '';
 if ( isset( $args['aria_label'] ) && ! empty( $args['aria_label'] ) ) {
 	$aria_label = 'aria-label="' . esc_attr( $args['aria_label'] ) . '"';
 }
+
+if(lang_en()) {
+	$placeholder = 'Search...';
+} elseif(lang_fr()) {
+	$placeholder = 'Recherche...';
+}
 ?>
 
 <form role="search" class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?>>
 	<label class="screen-reader-text" for="<?php echo $uid; ?>"><?php echo esc_html_x( 'Search for:', 'label', 'understrap' ); ?></label>
 	<div class="input-group">
-		<input type="search" class="px-3 w-100 form-control border-primary bg-white rounded" id="<?php echo $uid; ?>" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder', 'understrap' ); ?>">
+		<input type="search" class="px-3 w-100 form-control border-primary bg-white rounded" id="<?php echo $uid; ?>" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr_x( $placeholder, 'placeholder', 'understrap' ); ?>">
 		<?php /* ?><input type="submit" class="submit search-submit btn btn-primary" name="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'understrap' ); ?>"><?php */ ?>
 	</div>
 </form>
