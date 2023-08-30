c883823ad0d71a67d86dc55812907f37de361e33
Pat Monette <patrick@ontarioseo.ca>
2023-08-30 15:01:20 -0400

    pdf button update on full reports

--- loop-templates/content-header.php
+++ loop-templates/content-header.php
@@ -68,7 +68,7 @@ $projects = apply_filters('wpml_object_id', 98, 'resource_type', false);
                     $pdf = get_field('pdf_link');
                 }
             ?>
-            <a class="btn btn-secondary" href="<?php echo esc_url($pdf); ?>" target="_blank" rel="noopener,noreferrer">View PDF Version <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-external-link.svg" alt="External Link"></a>
+            <a class="btn btn-secondary px-3 py-2" href="<?php echo esc_url($pdf); ?>" target="_blank" rel="noopener,noreferrer">View PDF Version <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-external-link.svg" alt="External Link"></a>
         <?php endif; ?>
 
     </div>
